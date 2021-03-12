4b84768b458e5e96d1737503bb088683dbab2b9c
Gustavo Cerqueira <gmcerqueira93@gmail.com>
2021-03-12 14:15:51 -0300

    Realizando requisito 6

--- script.js
+++ script.js
@@ -1,8 +1,9 @@
 const colorAnswer = document.querySelector('#rgb-color');
 const colorsContainer = document.querySelector('#colors-container');
 const answerText = document.querySelector('#answer');
+const resetBtn = document.querySelector('#reset-game');
 
-// Criando core aleatória
+// Cria cor aleatória
 function randomColor() {
   const r = Math.floor(Math.random() * 255);
   const g = Math.floor(Math.random() * 255);
@@ -11,6 +12,7 @@ function randomColor() {
   return `(${r}, ${g}, ${b})`;
 }
 
+// Verifica se a resposta está correta
 function rightGuess(event) {
   const guess = event.target.id;
   const check = guess === 'rightAnswer';
@@ -18,6 +20,7 @@ function rightGuess(event) {
   else answerText.innerHTML = 'Errou! Tente novamente!';
 }
 
+// Cria opções de cores
 function createColorBalls(numberOfColors) {
   for (let i = 0; i < numberOfColors; i += 1) {
     const ball = document.createElement('div');
@@ -28,6 +31,7 @@ function createColorBalls(numberOfColors) {
   }
 }
 
+// seleciona resposta correta mostra a cor a ser adivinhada
 function selectingAnswer() {
   const balls = document.querySelectorAll('.ball');
   const colors = [];
@@ -37,5 +41,15 @@ function selectingAnswer() {
   colorAnswer.innerHTML = colors[randomIndex].slice(3);
 }
 
-createColorBalls(6);
-selectingAnswer();
+function resetGame() {
+  colorsContainer.innerHTML = '';
+  createColorBalls(6);
+  selectingAnswer();
+  answerText.innerHTML = 'Escolha uma cor';
+}
+
+window.onload = () => {
+  createColorBalls(6);
+  selectingAnswer();
+  resetBtn.addEventListener('click', resetGame);
+};
