22c4d236dbaec748642586e13d3e12d539896ee0
denis-rossati <denis.rossati@hotmail.com>
2021-03-12 12:49:47 -0300

    requisito 2 - tentativa

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-function requisito1 () {
+function requisito1() {
   document.body.appendChild(document.createElement('h1'));
   document.getElementsByTagName('h1')[0].id = 'title';
   document.getElementsByTagName('h1')[0].innerText = 'Color Guess - O jogo.';
 }
 
-window.onload = function init () {
-  requisito1();
+function requisito2() {
+  const rgbP = document.createElement('p');
+  document.body.appendChild(rgbP);
+  rgbP.innerText = `(168, 34, 1)`;
+  rgbP.id = 'rgb-color';
 }
+
+window.onload = function init() {
+  requisito1();
+  requisito2();
+};
