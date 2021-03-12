e7a83bb076df88d566526f41a331d6d34ba01ca9
denis-rossati <denis.rossati@hotmail.com>
2021-03-12 13:01:54 -0300

    requisito 3 - tentativa

--- script.js
+++ script.js
@@ -7,11 +7,24 @@ function requisito1() {
 function requisito2() {
   const rgbP = document.createElement('p');
   document.body.appendChild(rgbP);
-  rgbP.innerText = `(168, 34, 1)`;
+  rgbP.innerText = '(168, 34, 1)';
   rgbP.id = 'rgb-color';
+}
+
+function requisito3() {
+  for (let x = 0; x < 6; x += 1) {
+    document.body.appendChild(document.createElement('div'));
+    document.body.lastChild.className = 'ball';
+    document.body.lastChild.style.width = '70px';
+    document.body.lastChild.style.height = '70px';
+    document.body.lastChild.style.border = '1px solid black';
+    document.body.lastChild.style.borderRadius = '50%';
+    document.body.lastChild.style.display = 'inline-block';
+  }
 }
 
 window.onload = function init() {
   requisito1();
   requisito2();
+  requisito3();
 };
