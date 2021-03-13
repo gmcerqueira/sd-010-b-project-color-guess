807cdf4a2f819c2c3f0e3440b926ec69639bd2a4
denis-rossati <denis.rossati@hotmail.com>
2021-03-13 05:23:36 -0300

    requisito 4 finalizado

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let rgbNumber = '';
 
 function generateRGB() {
   const randomNumber = [];
-  for(let x = 0; x < 3; x += 1) {
+  for (let x = 0; x < 3; x += 1) {
     randomNumber.push(Math.ceil(Math.random() * (255 - 0) + 0));
   }
   rgbNumber = `(${randomNumber[0]},${randomNumber[1]},${randomNumber[2]})`;
@@ -13,7 +13,7 @@ function generateRGB() {
 function colorCircles() {
   for (let x = 0; x < balls.length; x += 1) {
     generateRGB();
-    balls[x].style.backgroundColor = ('rgb' + rgbNumber);
+    balls[x].style.backgroundColor = (`rgb${rgbNumber}`);
   }
 }
 
