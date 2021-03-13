3913788f8d25cdb90a506d2917b7b585afa6eac0
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-13 18:32:53 -0300

    4 - Adicione cores nas bolas elas devem ser geradas dinâmicamente

--- script.js
+++ script.js
@@ -8,3 +8,9 @@ console.log(number);
 
 const rgb = `(${getNumberRandom()}, ${getNumberRandom()}, ${getNumberRandom()})`
 pRGB.innerText = rgb;
+
+balls = document.getElementsByClassName('ball');
+
+for (let index = 0; index < balls.length; index +=1 ){
+  balls[index].style.backgroundColor = `rgb(${getNumberRandom()}, ${getNumberRandom()}, ${getNumberRandom()})`;
+}
