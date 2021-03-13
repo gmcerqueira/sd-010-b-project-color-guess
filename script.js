1725b8902dbdecb1bf7ffc0e7fd77603920c9f76
denis-rossati <denis.rossati@hotmail.com>
2021-03-13 05:22:48 -0300

    requisito 4 finalizado

--- script.js
+++ script.js
@@ -1,13 +1,22 @@
+const balls = document.getElementsByClassName('ball');
+let rgbNumber = '';
+
 function generateRGB() {
-  let rgbNumber = '';
   const randomNumber = [];
-  for (let x = 0; x < 3; x += 1) {
+  for(let x = 0; x < 3; x += 1) {
     randomNumber.push(Math.ceil(Math.random() * (255 - 0) + 0));
   }
   rgbNumber = `(${randomNumber[0]},${randomNumber[1]},${randomNumber[2]})`;
   document.querySelector('p').innerText = rgbNumber;
 }
 
+function colorCircles() {
+  for (let x = 0; x < balls.length; x += 1) {
+    generateRGB();
+    balls[x].style.backgroundColor = ('rgb' + rgbNumber);
+  }
+}
+
 window.onload = function loadPage() {
-  generateRGB();
+  colorCircles();
 };
