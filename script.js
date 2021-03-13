afb0110354250045661a88d03298a366581317d4
Roger Franco <rogerfrancogalt@gmail.com>
2021-03-13 18:30:30 -0400

    requisito 5

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-/* exercicio 4 */
-
 const rgbCode = document.getElementById('rgb-color')
 const balls = document.querySelectorAll('.ball')
 
@@ -9,13 +7,28 @@ function rgbColor() {
     let blue = Math.floor(Math.random()*256)
     rgbCode.innerHTML = '('+red+','+green+','+blue+')'
     balls[0].style.backgroundColor = 'rgb('+red+','+green+','+blue+')'
+    balls[0].addEventListener('click', function () {
+        document.getElementById('answer').innerHTML = 'Acertou!'
+    })
     balls[1].style.backgroundColor = 'rgb('+green+','+blue+','+red+')'
+    balls[1].addEventListener('click', function () {
+        document.getElementById('answer').innerHTML = 'Errou! Tente novamente!'
+    })
     balls[2].style.backgroundColor = 'rgb('+blue+','+red+','+green+')'
+    balls[2].addEventListener('click', function () {
+        document.getElementById('answer').innerHTML = 'Errou! Tente novamente!'
+    })
     balls[3].style.backgroundColor = 'rgb('+red+','+blue+','+green+')'
+    balls[3].addEventListener('click', function () {
+        document.getElementById('answer').innerHTML = 'Errou! Tente novamente!'
+    })
     balls[4].style.backgroundColor = 'rgb('+green+','+red+','+blue+')'
+    balls[4].addEventListener('click', function () {
+        document.getElementById('answer').innerHTML = 'Errou! Tente novamente!'
+    })
     balls[5].style.backgroundColor = 'rgb('+blue+','+green+','+red+')'
+    balls[5].addEventListener('click', function () {
+        document.getElementById('answer').innerHTML = 'Errou! Tente novamente!'
+    })
 }
 rgbColor()
-
-
-
