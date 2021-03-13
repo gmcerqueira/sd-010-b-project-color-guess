fb996b25aebfe56a34b8ec58bf8578e11e0c4009
ViniciusAlvesRocha <vinialvesrocha@gmail.com>
2021-03-13 20:32:18 -0300

    7 - Crie um placar que incremente 3 pontos para cada acerto no jogo

--- script.js
+++ script.js
@@ -1,16 +1,70 @@
 pRGB = document.getElementById('rgb-color');
 
+pFeedback = document.getElementById('answer');
+pFeedback.innerText = 'Escolha uma cor';
+
 function getNumberRandom() {
-    return Math.ceil(Math.random()*255);
+    return Math.ceil(Math.random() * 255);
 }
 const number = getNumberRandom();
-console.log(number);
 
 const rgb = `(${getNumberRandom()}, ${getNumberRandom()}, ${getNumberRandom()})`
 pRGB.innerText = rgb;
 
+
 balls = document.getElementsByClassName('ball');
 
-for (let index = 0; index < balls.length; index +=1 ){
-  balls[index].style.backgroundColor = `rgb(${getNumberRandom()}, ${getNumberRandom()}, ${getNumberRandom()})`;
+function setBallsWithIdNull() {
+    for (let index = 0; index < balls.length; index += 1) {
+        balls[index].setAttribute('id', '');
+    }
 }
+
+const score = document.getElementById('score');
+let scoreValue = 0;
+
+function setAnswer(ball) {
+    pFeedback = document.getElementById('answer');
+    pFeedback.innerText = '';
+    if (ball.target.style.backgroundColor == `rgb${pRGB.innerText}`) {
+        pFeedback.innerText = 'Acertou!';
+        score.innerText = scoreValue += 3;
+    } else {
+        pFeedback.innerText = 'Errou! Tente novamente!';
+    }
+}
+
+let ballSelected = Math.ceil(Math.random() * 6);
+console.log(ballSelected);
+for (let index = 1; index <= balls.length; index += 1) {
+    if (index === ballSelected) {
+        balls[index-1].style.backgroundColor = `rgb${rgb}`;
+    } else {
+        balls[index-1].style.backgroundColor = `rgb(${getNumberRandom()}, ${getNumberRandom()}, ${getNumberRandom()})`;
+    }
+    balls[index-1].addEventListener('click', setAnswer)
+}
+
+/* 6 - Reiniciar jogo */
+
+
+function resetGame() {
+    let rgb = `(${getNumberRandom()}, ${getNumberRandom()}, ${getNumberRandom()})`
+    pRGB.innerText = rgb;
+
+    let ballSelected = Math.ceil(Math.random() * 6);
+console.log(ballSelected);
+for (let index = 1; index <= balls.length; index += 1) {
+    if (index === ballSelected) {
+        balls[index-1].style.backgroundColor = `rgb${rgb}`;
+    } else {
+        balls[index-1].style.backgroundColor = `rgb(${getNumberRandom()}, ${getNumberRandom()}, ${getNumberRandom()})`;
+    }
+    balls[index-1].addEventListener('click', setAnswer)
+}
+pFeedback = document.getElementById('answer');
+    pFeedback.innerText = 'Escolha uma cor';
+}
+buttonReset = document.getElementById('reset-game');
+
+buttonReset.addEventListener('click', resetGame);
