d41ab2c1f82e6721c1c4ae38d8c4472e4c337bfb
denis-rossati <denis.rossati@hotmail.com>
2021-03-13 15:11:35 -0300

    requisito 5 - tentativa

--- script.js
+++ script.js
@@ -7,7 +7,6 @@ function generateRGB() {
     randomNumber.push(Math.ceil(Math.random() * (255 - 0) + 0));
   }
   rgbNumber = `(${randomNumber[0]},${randomNumber[1]},${randomNumber[2]})`;
-  document.querySelector('p').innerText = rgbNumber;
 }
 
 function colorCircles() {
@@ -17,6 +16,36 @@ function colorCircles() {
   }
 }
 
+function chooseRandomCircle() {
+  const chooseCircle = (Math.ceil(Math.random() * (5 - 0) + 0));
+  balls[chooseCircle].id = 'answer';
+}
+
+function getRgbNumber() {
+  let rgbText = '';
+  const answer = document.getElementById('answer');
+  for (let counter = 3; counter < answer.style.backgroundColor.length; counter += 1) {
+    rgbText += answer.style.backgroundColor[counter];
+  }
+  document.querySelector('p').innerText = rgbText;
+}
+
+function gameStart() {
+  alert('Escolha uma cor');
+  document.getElementById('circle-container').addEventListener('click', (event) => {
+    const eventTargetColor = event.target.style.backgroundColor;
+    const answerColor = document.getElementById('answer').style.backgroundColor;
+    if (eventTargetColor === answerColor) {
+      alert('Acertou!');
+    } else {
+      alert('Errou! Tente novamente!');
+    }
+  });
+}
+
 window.onload = function loadPage() {
   colorCircles();
+  chooseRandomCircle();
+  getRgbNumber();
+  gameStart();
 };
