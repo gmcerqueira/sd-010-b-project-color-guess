ab64327e85b4538c01f16230db5ac835bdf3e256
Gabriel Miranda <gabriel.mirandab92@gmail.com>
2021-03-15 10:52:26 -0300

    add exercicio 3 e 4

--- script.js
+++ script.js
@@ -1,10 +1,20 @@
 const rgbRandom = document.getElementById('rgb-color');
+const backgroundColorBalls = document.getElementsByClassName('ball');
 
 function generateRandomColor() {
-  const color1 = parseInt(Math.random() * 255);
-  const color2 = parseInt(Math.random() * 255);
-  const color3 = parseInt(Math.random() * 255);
+  const color1 = parseInt(Math.random() * 255, 0);
+  const color2 = parseInt(Math.random() * 255, 0);
+  const color3 = parseInt(Math.random() * 255, 0);
 
   rgbRandom.innerHTML = `(${color1}, ${color2}, ${color3})`;
 }
 generateRandomColor();
+
+function generateRandomColorsBalls() {
+  for (let index = 0; index < backgroundColorBalls.length; index += 1) {
+    backgroundColorBalls[index].style.backgroundColor = `rgb(${parseInt(Math.random() * 255, 0)}, 
+    ${parseInt(Math.random() * 255, 0)}, 
+    ${parseInt(Math.random() * 255, 0)})`;
+  }
+}
+generateRandomColorsBalls();
