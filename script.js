0ef37f1270ba3c5db3ba7420794ab028d86dd035
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-03-16 19:59:51 -0300

    Requisito 5

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 function randomRGB() {
   const red = Math.floor(Math.random() * 256);
   const green = Math.floor(Math.random() * 256);
@@ -13,6 +11,17 @@ function genColors() {
   for (let i = 0; i < balls.length; i += 1) {
     balls[i].style.backgroundColor = randomRGB();
   }
+  const colorToGuess = balls[Math.floor(Math.random() * 6)].style.backgroundColor;
+  document.getElementById('rgb-color').innerText = colorToGuess
 }
-
 genColors();
+
+document.addEventListener('click', (event) => {
+  if (event.target.classList.contains('ball')) {
+    if (document.getElementById('rgb-color').innerText == event.target.style.backgroundColor) {
+      document.getElementById('answer').innerText = 'Acertou!';
+    } else {
+      document.getElementById('answer').innerText = 'Errou! Tente novamente!';
+    }
+  }
+});
