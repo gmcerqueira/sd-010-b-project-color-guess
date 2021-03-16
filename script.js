ca4468bb805aaadb3e66a5a58edc08a1b7a36fb1
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-03-16 19:28:23 -0300

    Correções

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-document.onload = genColors();
+
 
 function randomRGB() {
-  const red = Math.floor(Math.random() * 256 );
-  const green = Math.floor(Math.random() * 256 );
-  const blue = Math.floor(Math.random() * 256 );
+  const red = Math.floor(Math.random() * 256);
+  const green = Math.floor(Math.random() * 256);
+  const blue = Math.floor(Math.random() * 256);
   const color = 'rgb(' + red + ', ' + green + ', ' + blue + ')';
   return color;
 }
 
 function genColors() {
-  let balls = document.getElementsByClassName('ball');
-  for (let ball of balls) {
-    ball.style.backgroundColor = randomRGB();
+  const balls = document.getElementsByClassName('ball');
+  for (let i = 0; i < balls.length; i += 1) {
+    balls[i].style.backgroundColor = randomRGB();
   }
 }
+
+genColors();
