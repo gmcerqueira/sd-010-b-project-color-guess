5b0a648467f1a2b8980086e45102abb9cf5ee23c
Paulo Henrique Lima <limapaulobsb@gmail.com>
2021-03-16 21:51:20 -0300

    Requisito bônus

--- script.js
+++ script.js
@@ -6,6 +6,8 @@ function randomRGB() {
   return color;
 }
 
+let firstTry;
+
 function genColors() {
   const balls = document.getElementsByClassName('ball');
   for (let i = 0; i < balls.length; i += 1) {
@@ -13,20 +15,34 @@ function genColors() {
   }
   const colorToGuess = balls[Math.floor(Math.random() * balls.length)].style.backgroundColor;
   document.getElementById('rgb-color').innerText = colorToGuess;
+  firstTry = true;
 }
+
 genColors();
+let score = 0;
 
 document.addEventListener('click', (event) => {
   if (event.target.classList.contains('ball')) {
-    if (document.getElementById('rgb-color').innerText == event.target.style.backgroundColor) {
+    if (document.getElementById('rgb-color').innerText === event.target.style.backgroundColor) {
       document.getElementById('answer').innerText = 'Acertou!';
+      if (firstTry) {
+        firstTry = false;
+        score += 3;
+      }
     } else {
       document.getElementById('answer').innerText = 'Errou! Tente novamente!';
+      if (firstTry) {
+        firstTry = false;
+        if (score > 0) {
+          score -= 1;
+        }
+      }
     }
+    document.getElementById('score').innerText = score;
   }
 });
 
-document. getElementById('reset-game').onclick = function () {
+document.getElementById('reset-game').onclick = function () {
   genColors();
-  document.getElementById('answer').innerText = 'Escolha uma cor!'
-}
+  document.getElementById('answer').innerText = 'Escolha uma cor!';
+};
